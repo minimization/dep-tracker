6a4795f6475185a38381294584d2dc713fa0d99d
Troy Dawson <tdawson@redhat.com>
2020-07-20 15:34:30 -0700

    create coreBuildRoot binaries and sources

--- buildroot-generator.py
+++ buildroot-generator.py
@@ -2,6 +2,7 @@
 
 import sys
 import argparse
+import copy
 import dnf
 import dnf.cli
 import dnf.exceptions
@@ -40,23 +41,58 @@ releasever = "33"
 listSources = []
 listSourcesDone = []
 listSourcesQueue = open(workDir + "packagelists-" + repoBase + "/Sources.all-arches").read().splitlines()
+#listSourcesQueue =['bash', 'sed']
 coreBuildRoot = ['bash', 'bzip2', 'coreutils', 'cpio', 'diffutils', 'fedora-release', 'findutils', 'gawk', 'glibc-minimal-langpack', 'grep', 'gzip', 'info', 'make', 'patch', 'redhat-rpm-config', 'rpm-build', 'sed', 'shadow-utils', 'tar', 'unzip', 'util-linux', 'which', 'xz']
+coreBuildRootBinaries = []
+coreBuildRootSources = []
 
 
 print(arch + ": Setup")
-base = dnf.Base()
-base.conf.read(repoConfDir + repoName + "." + arch + ".repo")
-base.conf.substitutions['releasever'] = releasever
-base.conf.installroot = installroot
-base.conf.arch = arch
-base.repos.enable_source_repos()
+baseCore = dnf.Base()
+baseCore.conf.read(repoConfDir + repoName + "." + arch + ".repo")
+baseCore.conf.substitutions['releasever'] = releasever
+baseCore.conf.installroot = installroot
+baseCore.conf.arch = arch
+baseCore.repos.enable_source_repos()
 
 #print(" Start read repos")
-base.read_all_repos()
+baseCore.read_all_repos()
 
 #print(" Start fill sack")
-base.fill_sack(load_system_repo=False)
+baseCore.fill_sack(load_system_repo=False)
 
+print(arch + ": Populating Core Buildroot")
+for this_binary in coreBuildRoot:
+  try:
+    baseCore.install(this_binary)
+  except dnf.exceptions.MarkingError:
+    print('    Cannot Install: ' + this_binary)
+## Resolve the coreBuildRoot
+try:
+  baseCore.resolve()
+  query = baseCore.sack.query().filterm(pkg=baseCore.transaction.install_set)
+  fileCoreBuildRootBinaries=open(outputDir + "CoreBuildRootBinaries", "a+")
+  fileCoreBuildRootSources=open(outputDir + "CoreBuildRootSources", "a+")
+  for pkg in query:
+    ## Put the binaries on the coreBuildRootBinaries list
+    if not pkg.name in coreBuildRootBinaries:
+      coreBuildRootBinaries.append(pkg.name)
+      fileCoreBuildRootBinaries.write("%s\n" % (pkg.name))
+    ## Put the source for the binary on the coreBuildRootSources list
+    ##   if it is not already there
+    if not pkg.source_name in coreBuildRootSources:
+      coreBuildRootSources.append(pkg.source_name)
+      fileCoreBuildRootSources.write("%s\n" % (pkg.source_name))
+      listSources.append(pkg.source_name)
+      listSourcesQueue.append(pkg.source_name)
+  fileCoreBuildRootBinaries.close()
+  fileCoreBuildRootSources.close()
+except dnf.exceptions.DepsolveError as e:
+  print("Core BuildRoot Resolution Error")
+  print(e)
+  		
+base = baseCore
+print(arch + ": Working on Source Queue")
 while 0 < len(listSourcesQueue):
   print('.', end='')
   #print('.', end='', flush=True)
@@ -67,7 +103,7 @@ while 0 < len(listSourcesQueue):
   thisBinaryList = []
   thisSourceList = []
   base.reset(goal='true')
-
+  
   ## Get the source as a pkg, from the package name
   # Although in rawhide there should only be one package with that name
   # Treat it as multi-packages, just incase
@@ -103,16 +139,18 @@ while 0 < len(listSourcesQueue):
       fileBinaryDeps=open(outputDir + "output/" + this_package + "-deps-binary", "a+")
       for pkg in query:
           ## Write the binary to the file
-          fileBinaryDeps.write("%s\n" % (pkg.name))
+          if not pkg.name in coreBuildRootBinaries:
+            fileBinaryDeps.write("%s\n" % (pkg.name))
 	  ## Put the source for the binary on the source lists
-	  ##   if it is not already there
-	  ## 2 source lists:  for this package, and overall
-          if not pkg.source_name in thisSourceList:
+	  ##   if it is not already there, or in coreBuildRoot
+	  ## 3 source lists:  for this package, overall, and queue
+          if not pkg.source_name in coreBuildRootSources:
+            if not pkg.source_name in thisSourceList:
               #print("  " + pkg.name + " : " + pkg.source_name)
               thisSourceList.append(pkg.source_name)
               if not pkg.source_name in listSources:
-                  listSources.append(pkg.source_name)
-                  listSourcesQueue.append(pkg.source_name)  
+                listSources.append(pkg.source_name)
+                listSourcesQueue.append(pkg.source_name)  
       fileBinaryDeps.close()
       
       fileSourceDeps=open(outputDir + "output/" + this_package + "-deps-source", "a+")
